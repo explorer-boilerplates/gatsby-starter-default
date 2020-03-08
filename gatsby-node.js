bb3453c2d43c5d83caa16d166aafbd0787387305
user <progexplorer@gmail.com>
2020-03-08 10:57:04 +0100

    chore(gatsby-node.js): delete comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
   // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
